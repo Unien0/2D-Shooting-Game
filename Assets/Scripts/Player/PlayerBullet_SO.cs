7618a70a562631a4a154ffdc0f496fffc6ed7923
UNIEN0 <unien7415@gmail.com>
2023-12-19 10:50:49 +0900

    【追加】玩家移动

---
+++ Assets/Scripts/Player/PlayerBullet_SO.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerBullet_SO", menuName = "Data/Bullet/PlayerBullet")]
+public class PlayerBullet_SO : ScriptableObject
+{
+    [Header("子弹资料")]
+    [Header("基础属性")]
+    [Header("_伤害")]
+    public int playerBulletDamage;//基础力量
+    [Header("_子弹冷却时间")]
+    public float linerBulletCoolDownTime;//基础
+    [Header("_存在时间")]
+    public float linerBulletExistenceTime;//存在时间
+
+
+    [Header("直线子弹")]
+    [Header("_速度")]
+    public float linerBulletSpeed;//基础
+    //public float linerBulletCurrentSpeed;//当前
+    [Header("_伤害倍率")]
+    public int linerBulletDamageMultipler;//子弹倍率
+    //public int linerBulletCurrentDamage;//当前
+    [Header("_穿透")]
+    public int linerBulletPenetrationCount;//穿透次数
+
+
+    [Header("追踪子弹")]
+    [Header("_速度")]
+    public float trackingBulletSpeed;//基础
+}
---
+++ Assets/Scripts/Player/PlayerData_SO.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData_SO", menuName = "Data/Player")]
+public class PlayerData_SO : ScriptableObject
+{
+    [Header("PlayerData")]
+    [Header("基本资料")]
+    public string playerName;//名称
+    public Sprite playerIcon;//图标
+    public Sprite playerOnWorldSprite;//游戏内画像
+    [Multiline] public string playerDescription;//介绍
+    [Space(10)]
+
+    [Header("玩家属性")]
+    public int playerMaxHP;//最大血量
+    public int playerHP;//当前血量
+    public int playerLucky;
+    [Header("_移动速度相关")]
+    public float playerBaseSpeed;//基础速度
+    public float playerCurrentSpeed;//当前速度
+     [Space(10)]
+
+    [Header("独立开关")]
+    public bool isDead;//是否死亡
+    public bool moveable;//是否可以移动
+    public bool controllable;//是否可以控制
+
+}
