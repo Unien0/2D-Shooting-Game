571d91dc275a591425e25ffe09d63d314e0736f5
UNIEN0 <unien7415@gmail.com>
2023-12-19 10:06:19 +0900

    【追加】新建文件夹，追加Pool、Audio等程序 【删除】Pool插件

---
+++ Assets/Scripts/Manager/AudioManager.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+
+public class AudioManager : Singleton<AudioManager>
+{
+    [Header("音乐SO")]
+    public SoundDetailsList_SO soundDetailsData;
+    public SceneSoundList_SO sceneSoundData;
+    [Header("Audio Source")]
+    public AudioSource ambientSource;
+    public AudioSource gameSource;
+
+    //随机播放时间
+    //public float MusicStartSecond => Random.Range(5f,15f);
+    //private Coroutine soundRoutine;
+
+    [Header("Audio Mixer")]
+    public AudioMixer audioMixer;
+
+    //AudioMixer状态
+    [Header("Snapshots")]
+    public AudioMixerSnapshot normalSnapShot;
+    public AudioMixerSnapshot muteSnapShot;
+
+    //音乐结束时间
+    private float musicTransitionSecond = 3f;
+
+
+    private void OnEnable()
+    {
+        EventHandler.AfterSceneLoadedEvent += OnAfterSceneLoadedEvent;
+        EventHandler.PlaySoundEvent += OnPlaySoundEvent;
+    }
+
+    private void OnDisable()
+    {
+        EventHandler.AfterSceneLoadedEvent -= OnAfterSceneLoadedEvent;
+        EventHandler.PlaySoundEvent -= OnPlaySoundEvent;
+    }
+
+    /// <summary>
+    /// 播放事件设定
+    /// </summary>
+    /// <param name="soundName"></param>
+    private void OnPlaySoundEvent(SoundName soundName)
+    {
+        var soundDetails = soundDetailsData.GetSoundDetails(soundName);
+        if (soundDetails != null)
+            EventHandler.CallInitSoundEffect(soundDetails);
+    }
+
+    /// <summary>
+    /// 场景BGM播放设置
+    /// </summary>
+    private void OnAfterSceneLoadedEvent()
+    {
+        //播放相关BGM
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        //获得对应场景BGM
+        SceneSoundItem sceneSound = sceneSoundData.GetSceneSoundItem(currentScene);
+
+        //防止BUG
+        if (sceneSound == null)
+            return;
+
+        //获取对应环境音和BGM
+        SoundDetails ambient = soundDetailsData.GetSoundDetails(sceneSound.ambient);
+        SoundDetails music = soundDetailsData.GetSoundDetails(sceneSound.music);
+
+        //音乐渐入渐出
+        PlayerAmbientClip(ambient,0.5f);
+        PlayerMusicClip(music,musicTransitionSecond);
+
+        ////延迟播放
+        //if (soundRoutine != null)
+        //{
+        //    StopCoroutine(soundRoutine);
+        //}
+        //else
+        //{
+        //    soundRoutine = StartCoroutine(PlaySoundRoutine(music, ambient));
+        //}
+    }
+
+    /// <summary>
+    /// 音乐播放
+    /// </summary>
+    /// <param name="soundDetails"></param>
+    private void PlayerMusicClip(SoundDetails soundDetails , float transitionTime)
+    {
+        //获取音频混合器的相应轨迹
+        audioMixer.SetFloat("MusicVolume", ConertSoundVolume(soundDetails.soundVolume));
+        gameSource.clip = soundDetails.soundClip;
+        if (gameSource.isActiveAndEnabled)
+            gameSource.Play();
+
+        normalSnapShot.TransitionTo(transitionTime);
+    }
+
+    /// <summary>
+    /// 播放环境音
+    /// </summary>
+    /// <param name="soundDetails"></param>
+    private void PlayerAmbientClip(SoundDetails soundDetails, float transitionTime)
+    {
+        audioMixer.SetFloat("AmbientVolume", ConertSoundVolume(soundDetails.soundVolume));
+        ambientSource.clip = soundDetails.soundClip;
+        if (gameSource.isActiveAndEnabled)
+            gameSource.Play();
+
+        normalSnapShot.TransitionTo(transitionTime);
+    }
+
+    //遅延播放
+    //private IEnumerator PlaySoundRoutine(SoundDetails music, SoundDetails ambient)
+    //{
+    //    if (music != null && ambient != null)
+    //    {
+    //        PlayerAmbientClip(ambient);
+    //        yield return new WaitForSeconds(MusicStartSecond);
+    //        PlayerMusicClip(music);
+    //    }
+    //}
+
+    //将音量调整到（-80,20）的范围区间
+    private float ConertSoundVolume(float amount)
+    {
+        return (amount * 100 - 80);
+    }
+}
---
+++ Assets/Scripts/Manager/PoolManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+//对象池
+public class PoolManager : MonoBehaviour
+{
+    //对象池内的物体
+    public List<GameObject> poolPrefabs;
+    private List<ObjectPool<GameObject>> poolEffectList = new List<ObjectPool<GameObject>>();
+    //声音堆栈
+    private Queue<GameObject> soundQueue = new Queue<GameObject>();
+
+    private void OnEnable()
+    {
+        EventHandler.InitSoundEffect += InitSoundEffect;
+    }
+    private void OnDisable()
+    {
+        EventHandler.InitSoundEffect -= InitSoundEffect;
+    }
+
+    /// <summary>
+    /// 生成对应物体
+    /// </summary>
+    private void CreateSoundPool()
+    {
+        var parent = new GameObject(poolPrefabs[0].name).transform;
+        parent.SetParent(transform);
+        //设置20个空物体
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject newObj = Instantiate(poolPrefabs[0], parent);
+            newObj.SetActive(false);
+            soundQueue.Enqueue(newObj);
+        }
+    }
+    /// <summary>
+    /// 获取音频
+    /// </summary>
+    /// <returns></returns>
+    private GameObject GetPoolObject()
+    {
+        if (soundQueue.Count < 2)
+            CreateSoundPool();
+        return soundQueue.Dequeue();
+    }
+
+    /// <summary>
+    /// 声音初始化
+    /// </summary>
+    /// <param name="soundDetails"></param>
+    private void InitSoundEffect(SoundDetails soundDetails)
+    {
+        var obj = GetPoolObject();
+        obj.GetComponent<Sound>().SetSound(soundDetails);
+        obj.SetActive(true);
+        StartCoroutine(DisableSound(obj, soundDetails.soundClip.length));
+    }
+
+    /// <summary>
+    /// 关闭声音播放
+    /// </summary>
+    /// <param name="obj"></param>
+    /// <param name="duration"></param>
+    /// <returns></returns>
+    private IEnumerator DisableSound(GameObject obj,float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        obj.SetActive(false);
+        soundQueue.Enqueue(obj);
+    }
+
+    /// <summary>
+    /// 如果需要播放声音的话填写以下内容
+    /// </summary>
+    private void CallMusic()
+    {
+        EventHandler.CallPlaySoundEvent(SoundName.none);
+    }
+}
