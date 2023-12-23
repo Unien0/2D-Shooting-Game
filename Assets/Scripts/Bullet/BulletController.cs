c30181a9244d5fe590f0b184ab05a4572e479376
UNIEN <unien7415@gmail.com>
2023-12-23 14:14:14 +0900

    【追加】手柄方向控制射击（完成，使用旧方法）

--- Assets/Scripts/Bullet/BulletController.cs
+++ Assets/Scripts/Bullet/BulletController.cs
@@ -6,14 +6,18 @@ public class BulletController : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public float bulletSpeed;
+    public float fireThreshold = 0.5f;
 
     void Update()
     {
-      
-        MousePosition();
-        BulletInstantiate();
+        //MousePosition();
+        //BulletInstantiate();
+        InputPosition();
     }
 
+    /// <summary>
+    /// 鼠标定位
+    /// </summary>
     void MousePosition()
     {
         // 获取鼠标在屏幕上的位置
@@ -38,6 +42,28 @@ public class BulletController : MonoBehaviour
             //GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
             //bullet.GetComponent<Rigidbody2D>().velocity = bulletSpeed * transform.right;
         }
+    }
+
+    /// <summary>
+    /// 手柄射击模式
+    /// </summary>
+    void InputPosition()
+    {
+        float horizontalInput = Input.GetAxis("RightStickHorizontal");
+        float verticalInput = Input.GetAxis("RightStickVertical");
+
+        if (Mathf.Abs(horizontalInput) > fireThreshold || Mathf.Abs(verticalInput) > fireThreshold)//设置死区（在一定值范围内不会射击，防止误触）
+        {
+            // 计算子弹的方向
+            Vector2 bulletDirection = new Vector2(horizontalInput, verticalInput).normalized;
+
+            // 创建子弹实例
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            // 设置子弹的速度和方向
+            bullet.GetComponent<Rigidbody2D>().velocity = bulletDirection * bulletSpeed;
+        }
+
     }
 
 }
