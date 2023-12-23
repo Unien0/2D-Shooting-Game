6a0b98d551419f782a4fc27a771469237a8fca43
UNIEN <unien7415@gmail.com>
2023-12-23 11:52:54 +0900

    【追加】手柄设置（未完

---
+++ Assets/Scripts/InputTest/InputTestSystem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTestSystem : MonoBehaviour
+{
+   public void PlayerMove()
+    {
+        Debug.Log("我移动了");
+    }
+}
