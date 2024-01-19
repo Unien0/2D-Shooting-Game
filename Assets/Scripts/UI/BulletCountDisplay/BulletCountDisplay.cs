cdf7d364a770097dacac9a08e8f357897b486cc7
UNIEN0 <unien7415@gmail.com>
2024-01-19 13:22:31 +0900

    【添加】子弹UI显示（未完

---
+++ Assets/Scripts/UI/BulletCountDisplay/BulletCountDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BulletCountDisplay : MonoBehaviour
+{
+    private TMP_Text countBullet;
+
+
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 子弹显示，int挂载子弹量
+    /// </summary>
+    /// <param name="remainingBullet"></param>
+    public void BulletDisplay(int remainingBullet)
+    {
+
+    }
+}
