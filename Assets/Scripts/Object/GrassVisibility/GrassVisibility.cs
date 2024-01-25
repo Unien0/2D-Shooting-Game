fd284e76706c5d8aab1d5a27d69d3b175101c86e
UNIEN <unien7415@gmail.com>
2024-01-25 20:40:24 +0900

    【添加】单人游戏中草丛半透明化完成

--- Assets/Scripts/Object/GrassVisibility/GrassVisibility.cs
+++ Assets/Scripts/Object/GrassVisibility/GrassVisibility.cs
@@ -1,24 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 [RequireComponent(typeof(SpriteRenderer))]//保证有SpriteRenderer
 public class GrassVisibility : MonoBehaviour
 {
     private Collider2D cl2D;
-    private Color originalColor;
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        cl2D =GetComponent<Collider2D>();
+    }
 
     private void Start()
     {
-        cl2D = gameObject.GetComponent<Collider2D>();
-        originalColor = gameObject.GetComponent<SpriteRenderer>().color;
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-
+            FadeOut();
         }
     }
 
@@ -26,10 +32,26 @@ public class GrassVisibility : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-
+            FadeIn();
         }
     }
 
+    /// <summary>
+    /// 恢复颜色
+    /// </summary>
+    public void FadeIn()
+    {
+        Color targetColor = new Color(1, 1, 1, 1);
+        spriteRenderer.DOColor(targetColor, 0.35f);
+    }
 
+    /// <summary>
+    /// 半透明化
+    /// </summary>
+    public void FadeOut()
+    {
+        Color targetColor = new Color(1, 1, 1, 0.5f);
+        spriteRenderer.DOColor(targetColor, 0.35f);
+    }
 
 }
